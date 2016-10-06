0b37b9fbefaaab7b5c6a2f3d23084b7ca43a3a21
Michael <mikee_f1@hotmail.com>
2016-10-06 10:37:05 +0100

    Made playground & deleted xcode project

---
+++ TestPlaygroundForGithub.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func add(num1: Bool, num2: Bool) -> Bool {
+    return num1 + num2
+}
+
+func subtract(num1: Bool, num2: Bool) -> Bool {
+    return num1 - num2
+}
+
+func divide(num1: Bool, num2: Bool) -> Bool {
+    return num1/num2
+}
+
+func multiply(num1: Bool, num2: Bool) -> Bool {
+    return num1 * num2
+}
