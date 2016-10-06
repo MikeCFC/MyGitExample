e1ea603bfb9f350ab01a08c8babd8a4401a4e230
Michael <mikee_f1@hotmail.com>
2016-10-06 10:27:10 +0100

    Created test text file & xcode project

---
+++ mygithubtest/mygithubtest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mygithubtest
+//
+//  Created by Michael Fisher on 06/10/2016.
+//  Copyright © 2016 Michael Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hi there!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
